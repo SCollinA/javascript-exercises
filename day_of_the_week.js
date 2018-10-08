c03169aa26cb089f17871c76de5e74c4965c50ca
Collin Argo <collinargo@gmail.com>
2018-10-08 14:09:35 -0400

    add error checking to user input

--- day_of_the_week.js
+++ day_of_the_week.js
@@ -3,6 +3,9 @@ let day = 0;
 while (true) {
     try {
         day = Number(prompt(`Day (0-6)? `));
+        if (isNaN(day) || day > 6 || day < 0) {
+            throw error;
+        }
         break
     } catch (error) {
         alert(`Please enter a number.`)
