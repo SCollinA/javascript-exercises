4229df868c4a8357fbc3b24c65424c8b04af81bf
Collin Argo <collinargo@gmail.com>
2018-10-11 10:58:35 -0400

    style page and enable sad facts on click

--- fortune_cookie/scripts/index.js
+++ fortune_cookie/scripts/index.js
@@ -1,8 +1,32 @@
-console.log('Yep, I am here')
+console.log('Yep, I am here');
 // Classes for styling
 // "data attributes" for JS
 
 // run code on button click
 // show text on click
 // pull data from array to provide text
-let buttonTrigger = document.querySelector();
+let buttonTrigger = document.querySelector('[data-trigger]');
+
+let divQuote = document.querySelector('[data-output]');
+
+let sadFacts = [
+    'Grizzlies have been known to abandon their cubs.',
+    'Humans killed the world\'s oldest animal.',
+    'Pluto didn\'t even get a birthday as a planet (i.e., make a full orbit around the sun).',
+    'Pigs can get depressed.',
+    'CPR doesn\'t usually work.',
+    'Scientists have found the loneliest whale.',
+    'The more you know, the worse it gets.'
+];
+// addEventListener accepts string for kind of event
+// and function to execute on event
+buttonTrigger.addEventListener('click', hello);
+
+let i = 0;
+
+function hello() {
+    let sadFact = sadFacts.pop();
+    console.log(`Hello addEventListener ${i}`);
+    divQuote.textContent = `${sadFact}`;
+    sadFacts.unshift(sadFact);
+}
