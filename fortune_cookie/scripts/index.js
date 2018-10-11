ef00b4975f2c2cb7631993f0481226322cecbb75
Collin Argo <collinargo@gmail.com>
2018-10-11 09:39:34 -0400

    add js file

---
+++ fortune_cookie/scripts/index.js
@@ -0,0 +1,8 @@
+console.log('Yep, I am here')
+// Classes for styling
+// "data attributes" for JS
+
+// run code on button click
+// show text on click
+// pull data from array to provide text
+let buttonTrigger = document.querySelector();
