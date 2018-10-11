b16412e2e08a21d9b8d5ddd3952fb0b32b07a34a
Collin Argo <collinargo@gmail.com>
2018-10-11 13:50:00 -0400

    add images and make sadFact random

--- fortune_cookie/scripts/index.js
+++ fortune_cookie/scripts/index.js
@@ -1,32 +1,49 @@
 console.log('Yep, I am here');
-// Classes for styling
-// "data attributes" for JS
+// // Classes for styling
+// // "data attributes" for JS
 
 // run code on button click
-// show text on click
-// pull data from array to provide text
-let buttonTrigger = document.querySelector('[data-trigger]');
+// // show text on click
+// // pull data from array to provide text
+let sadTrigger = document.querySelector('[data-trigger="sadFacts"]');
 
-let divQuote = document.querySelector('[data-output]');
+let sadQuote = document.querySelector('[data-output="sadFacts"]');
+
+let sadImage = document.querySelector('[data-output-image]');
 
 let sadFacts = [
-    'Grizzlies have been known to abandon their cubs.',
-    'Humans killed the world\'s oldest animal.',
-    'Pluto didn\'t even get a birthday as a planet (i.e., make a full orbit around the sun).',
-    'Pigs can get depressed.',
-    'CPR doesn\'t usually work.',
-    'Scientists have found the loneliest whale.',
-    'The more you know, the worse it gets.'
+    { text: 'Grizzlies have been known to abandon their cubs.', image: 'images/sad_bear.jpg'}, { text: 'Humans killed the world\'s oldest animal.', image: 'images/sad_dino.jpg'}, { text: 'Pluto didn\'t even get a birthday as a planet (i.e., make a full orbit around the sun).', image: 'images/sad_planet.jpg'}, { text: 'Pigs can get depressed.', image: 'images/sad_pig.jpg'}, { text: 'CPR doesn\'t usually work.', image: 'images/sad_cpr.jpg'}, { text: 'Scientists have found the loneliest whale.' , image: 'images/sad_whale.jpg'}, { text: 'The more you know, the worse it gets.', image: 'images/sad_asteroid.jpg'}
 ];
-// addEventListener accepts string for kind of event
-// and function to execute on event
-buttonTrigger.addEventListener('click', hello);
 
-let i = 0;
+let lottoTrigger = document.querySelector('[data-trigger="lottoNumbers"]');
 
-function hello() {
-    let sadFact = sadFacts.pop();
-    console.log(`Hello addEventListener ${i}`);
-    divQuote.textContent = `${sadFact}`;
-    sadFacts.unshift(sadFact);
+let lottoQuote = document.querySelector('[data-output="lottoNumbers"]');
+
+// let lottoNumbers = [
+//     '10 20 30 40 50 60',
+//     '20 30 40 50 60 10',
+//     '30 40 50 60 10 20'
+// ];
+// // addEventListener accepts string for kind of event
+// // and function to execute on event
+sadTrigger.addEventListener('click', makeSadFact);
+
+function makeSadFact() {
+    let sadFact = sadFacts[Math.floor(Math.random() * sadFacts.length)]
+    // sadFact = sadFacts.pop();
+    console.log(`Hello addEventListener`);
+    sadQuote.textContent = `${sadFact['text']}`;
+    sadImage.setAttribute('src', `${sadFact['image']}`)
+    // sadFacts.unshift(sadFact);
+}
+
+lottoTrigger.addEventListener('click', makeLottoNumbers);
+
+function makeLottoNumbers() {
+    let lottoNumbers = [];
+    for (i = 6; i > 0; i--) {
+        lottoNumber = Math.floor((Math.random() * 100));
+        lottoNumbers.push(lottoNumber);
+    }
+    lottoQuote.textContent = lottoNumbers.join(', ');
 }
